def24a98d357f3fe6293ee10876b96047829e460
monyorojoseph <monyorojoseph@gmail.com>
2024-03-30 14:26:05 +0300

    removed prisma

--- app/projects/drafts/page.tsx
+++ app/projects/drafts/page.tsx
@@ -1,16 +1,4 @@
-import prisma from "@/lib/prisma";
-
-async function getData() {
-
-    const feed = await prisma.project.findMany({
-        where: { published: false }
-    });
-    return feed
-}
-
 const DraftProjects = ()=> {
-    const feed = getData()
-
     return(
         <section>Draft Projects</section>
     )
